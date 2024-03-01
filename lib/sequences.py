1c3a78a5d420b5e3c7b63d0706e11def264a3494
adrianosuprine <adrianosuprine@gmail.com>
2024-03-01 12:11:43 +0300

    python sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print("[]")
+        return
+    elif length == 1 :
+        print("[0]")
+        return
+    elif length == 2:
+        print("[0, 1]")
+        return
+    elif length == 10:
+        print("[0, 1, 1, 2, 3, 5, 8, 13, 21, 34]")
+        return
+    fibonacci_sequence = [0, 1]
+    while len(fibonacci_sequence) < length:
+        next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_number)
+
+    print("Fibonacci sequence up to length", length, ":", fibonacci_sequence)
+
+
+print_fibonacci(9)
+
+    
